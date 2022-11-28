d91b57536c4ed66cfeee8e303056d58679002b3d
doobyeol <doobyeol@gmail.com>
2022-11-29 00:32:26 +0900

    FIX: ask store js -> ts

--- src/store/ask.ts
+++ src/store/ask.ts
@@ -2,35 +2,40 @@ import { defineStore } from 'pinia'
 import { ref, onBeforeMount, reactive, computed } from "vue";
 
 export const useAskStore = defineStore('askStore', () => {
-  const askItem = ref();
+  interface item {
+    key: number
+    class: string
+    text: string
+  }
+  const askItem = ref({});
   const asks = ref([
     { key: 0, class: 'right', text: "안녕하세요!" },
     { key: 1, class: 'right', text: "취미가 뭔가요?" },
-    { key: 2, class: 'right', text: "희망연봉은 얼마인가요?" },
+    { key: 2, class: 'right', text: "개발자가 된 이유는 뭔가요?" },
     { key: 3, class: 'right', text: "꿈이 뭔가요?" },
     { key: 4, class: 'right', text: "잘했다고 생각하는 프로젝트에 대해 알려 주세요!" },
   ]);
 
   const answers = ref([
     { key: 0, class: 'left', text: "이렇게 귀한곳에 귀한분이..! 반갑습니다!" },
-    { key: 1, class: 'left', text: "주말에 분조카 가서 카공하는걸 좋아합니다 ㅎㅎ" },
-    { key: 2, class: 'left', text: "회사 내규에 따르겠습니다..!" },
-    { key: 3, class: 'left', text: "서른전에 풀스택 개발자가 되는 것입니다." },
-    { key: 4, class: 'left', text: "현재 진행중인 프로젝트는 ..." },
+    { key: 1, class: 'left', text: "저는 주말마다 카페가서 코딩하는 걸 좋아해요!" },
+    { key: 2, class: 'left', text: "재밌어서요!" },
+    { key: 3, class: 'left', text: "풀스택 개발자가 되는 것입니다." },
+    { key: 4, class: 'left', text: "잘했다고 생각하는 프로젝트는 ..." },
   ]);
 
-  const chatItems = ref([]);
+  const chatItems = ref<Array<item>>([]);
 
   const initAsks = () => {
     askItem.value = {};
     chatItems.value = [];
   }
 
-  const registerAsk = (item) => {
+  const registerAsk = (item: item) => {
     askItem.value = item;
     chatItems.value.push(item);
 
-    answers.value.forEach((answer) => {
+    answers.value.forEach((answer: item) => {
       if (answer.key == item.key) {
         chatItems.value.push(answer);
       }
@@ -40,11 +45,11 @@ export const useAskStore = defineStore('askStore', () => {
     removeAnswer(item);
   }
 
-  const removeAsk = (item) => {
+  const removeAsk = (item: item) => {
     asks.value = asks.value.filter((ask) => ask.key != item.key);
   }
 
-  const removeAnswer = (item) => {
+  const removeAnswer = (item: item) => {
     answers.value = answers.value.filter((answer) => answer.key != item.key);
   }
 
