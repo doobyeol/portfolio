541eafb97d657b855074df3fe799aa2f528cace3
Doobyeol <doobyeol@gmail.com>
2022-12-11 00:06:59 +0900

    ADD : 기술 뱃지 컴포넌트 추가

---
+++ src/components/Badge.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+defineProps<{
+  items: Array<{
+    value: string,
+    color: string,
+    logo: string
+  }>;
+}>();
+
+</script>
+
+<template>
+  <ul class="badge">
+    <li 
+      v-for="item in items" 
+      :key="item.value" 
+      :style="{ backgroundColor : item.color }"
+    >
+      <img :src="item.logo" :style="{ width : item.logo ? '13px' : '0px' }"/>
+      {{ item.value }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.badge {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.badge li {
+    width: fit-content;
+    border-radius: 7px;
+    color: #fff;
+    margin: 8px 9px 0 0;
+    padding: 3px 11px;
+    line-height: 19px;
+    vertical-align: middle;
+    font-weight: 600;
+    font-size: 0.9em;
+    font-family: '모리스9';
+}
+
+img {
+  filter: invert(1);
+  display: inline-block;
+  width: 13px;
+  position: relative;
+  top: 3px;
+}
+</style>
--- src/components/PixArticle.vue
+++ src/components/PixArticle.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { storeToRefs } from "pinia";
 import { useTimelineStore, ArticleType } from "../store/timeline";
-
+import Badge from "@/components/Badge.vue";
 const timelineStore = useTimelineStore();
 
 const { fixedArticle } = storeToRefs(timelineStore);
+
+const skillsBadge = ref<Array<any>>([
+  {value: 'Java', color: '#5283a2'},
+  {value: 'Spring', color: '#62a458', logo: 'https://simpleicons.org/icons/spring.svg'},
+  {value: 'Oracle', color: '#ae2525', logo: 'https://simpleicons.org/icons/oracle.svg'},
+  {value: 'PostgreSQL', color: '#526e8e', logo: 'https://simpleicons.org/icons/postgresql.svg'},
+  {value: 'Node.js', color: '#759c60', logo: 'https://simpleicons.org/icons/nodedotjs.svg'},
+  {value: 'Vue.js', color: '#11b48a', logo: 'https://simpleicons.org/icons/vuedotjs.svg'},
+  {value: 'JavaScript', color: '#ce8500', logo: 'https://simpleicons.org/icons/javascript.svg'},
+  {value: 'TypeScript', color: '#1c63b3', logo: 'https://simpleicons.org/icons/typescript.svg'},
+  {value: 'SCSS', color: '#c9469d', logo: 'https://simpleicons.org/icons/sass.svg'},
+  {value: 'Git', color: '#3e3e4a', logo: 'https://simpleicons.org/icons/git.svg'},
+  {value: 'Docker', color: '#0090db', logo: 'https://simpleicons.org/icons/docker.svg'},
+]);
 
 </script>
 
@@ -12,11 +27,14 @@ const { fixedArticle } = storeToRefs(timelineStore);
   <div class="timelineWrap" v-if="fixedArticle">
     <div class="content">
       <div class="profile">
-        <div class="img" />
-        <p class="name">두별</p>
+        <!-- <div class="img" />
+        <p class="name">두별</p> -->
+        <b> {{ fixedArticle.title }} </b>
       </div>
       <div class="column">
         <p class="preLine">
+          <!-- <b> {{ fixedArticle.title }} </b> -->
+          <Badge :items="skillsBadge" />
           {{ fixedArticle.text }}
           <a v-if="fixedArticle.link" target="_blink" :href="fixedArticle.link">
             {{ fixedArticle.link }}
@@ -33,6 +51,21 @@ const { fixedArticle } = storeToRefs(timelineStore);
 </template>
 
 <style scoped>
+
+.bedge {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.bedge li {
+    width: fit-content;
+    border-radius: 7px;
+    color: #fff;
+    background: rgb(108, 115, 115);
+    margin: 8px 9px 0 0;
+    padding: 4px 10px 7px 10px;
+    vertical-align: middle;
+}
 
 .preLine {
   white-space: pre-line;
@@ -57,6 +90,10 @@ const { fixedArticle } = storeToRefs(timelineStore);
   display: flex;
   position: relative;
 }
+
+.timelineWrap .profile b{
+  padding-left: 27px;
+}
 .timelineWrap .profile .name {
   line-height: 35px;
   padding-left: 10px;
@@ -77,8 +114,8 @@ const { fixedArticle } = storeToRefs(timelineStore);
   width: 20px;
   height: 20px;
   position: absolute;
-  top: -10px;
-  right: -4px;
+  top: 0px;
+  left: -4px;
   background-image: url(/src/assets/img/content/push_pin.png);
   background-position: center;
   background-size: cover;
